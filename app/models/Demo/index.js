cf8134d9cb9594522f60534d798f86854bc09eed
g2060701343@163.com <gxj53264>
2018-01-15 16:18:58 +0800

    保存token

--- app/models/Demo/index.js
+++ app/models/Demo/index.js
@@ -1,6 +1,9 @@
 import { routerRedux, Route, Switch } from 'dva/router';
 import router from '../../routes';
 import *as service from '../../services/CaptainService.js'
+import React, {
+	     AsyncStorage
+	 }from 'react-native';
 const { ConnectedRouter } = routerRedux;
 export default {
 	namespace: 'demo',
@@ -25,10 +28,34 @@ export default {
 			console.log(param)
 			const res = yield call(service.login,param)
 			console.log(res)	
+			yield put({
+				type:'saveToken',
+				payload:res.data.token
+			})
 		},
 	
 	},
 	reducers: {
+		saveToken(state,{payload}){
+			AsyncStorage.setItem("token",payload).then(
+				()=>{   //成功的操作
+					console.log("token保存成功!");
+				},
+			);
+			AsyncStorage.getItem('token')
+				.then(  //使用Promise机制的方法
+					(result)=> {   //使用Promise机制,如果操作成功不会有error参数
+						if (result == null) {
+							//没有指定的key
+							return;
+						}
+						console.log("token:" + result);
+					}
+				).catch((error)=> {  //读取操作失败
+					console.log('error:' + error.message);
+				});
+			return {...state}
+		}
     }
 
 };
